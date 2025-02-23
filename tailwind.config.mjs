b92e91c16855c1c99088b1856315741dc052308c
SonalPathinayaka <sonalpathinayaka7@gmail.com>
2025-02-23 20:44:48 +0530

    first

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,25 @@
+// tailwind.config.js
+const {heroui} = require("@heroui/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
+        // logoColor: 'rgb(118, 199, 209)',
+        logoColor: 'rgb(50, 105, 130)',
+        heroColor: 'rgb(150, 200, 220)',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
